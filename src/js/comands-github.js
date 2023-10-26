8f7f62c47e7ca9cda367ce7e043a484523c362e7
Vladimir Kovalenko <dante77758@gmail.com>
2023-10-26 20:50:19 +0300

    add modification file

--- src/js/comands-github.js
+++ src/js/comands-github.js
@@ -1,4 +1,6 @@
 import { comandsItems } from './items/comands-github-items';
 import createRenderingItems from './function-rendering-Items';
 
+const UlEl = document.getElementById('hero-list');
+
 UlEl.insertAdjacentHTML('beforeend', createRenderingItems(comandsItems));
